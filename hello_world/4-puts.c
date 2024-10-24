5d51d70346fe03267bfe61eb6c3da6898aad91bf
atakhan11 <atakhanwork@gmail.com>
2024-10-24 21:46:13 +0400

    hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry point of the program
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+{
+    printf("Programming is like building a multilingual puzzle\n");
+    return (0);
 }
+
