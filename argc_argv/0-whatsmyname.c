3b76bb3e20c3f36109dcff2baa837b3356dab416
atakhan11 <atakhanwork@gmail.com>
2024-11-15 18:23:36 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include <stdio.h>		
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  *  main - Main Entry
@@ -7,8 +7,9 @@
  *  @argv: array of pointers to those strings.
  *  *Return: Success point 0.
  */
-int main(int argc __attribute__((unused)), char *argv[])		
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
-}	
+}
