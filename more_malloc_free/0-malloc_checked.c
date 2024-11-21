95bc025d95373f94b94465e597970dc40b8beace
atakhan11 <atakhanwork@gmail.com>
2024-11-21 21:08:16 +0000

    d

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
-#include "stdlib.h"
+#include <stdlib.h>
+
 /**
  *malloc_checked - function
  * @b: variable
@@ -8,8 +9,9 @@
 void *malloc_checked(unsigned int b)
 {
 	void *t;
+
 	t = malloc(b);
 	if (t == NULL)
 		exit(98);
-	return (t);	
+	return (t);
 }	
