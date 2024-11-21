37ea79d3dcbeeedc95905c545e9e18f6a35189d0
atakhan11 <atakhanwork@gmail.com>
2024-11-21 07:53:27 +0000

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _strdup - copy and return input str
+ * @str: - entered string
+ * Return: Every element of array
+ */
+char *_strdup(char *str)
+{
+	int i,length = 0;
+	char *arr;
+	if(str == NULL)
+		return(NULL);
+	while(str[length])
+		length++;
+
+	arr = malloc((length + 1) * sizeof(char));
+	if(arr == NULL)
+		return (0);
+	for(i = 0, i < length, i++)
+		arr[1] = str[1]
+	if(arr == NULL)
+		return(NULL);
+	return(arr);
+}	
