a521b90ec4b260ad0421f012fc9ba495e70e4feb
atakhan11 <atakhanwork@gmail.com>
2024-11-23 12:47:33 +0000

    3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - adds the numbers
+ * @a: - first number
+ * @b: - second number
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subs the numbers
+ * @a: - first number
+ * @b: - second number
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - muls the numbers
+ * @a: - first number
+ * @b: - second number
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divs the numbers
+ * @a: - first number
+ * @b: - second number
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - mods the numbers
+ * @a: - first number
+ * @b: - second number
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
