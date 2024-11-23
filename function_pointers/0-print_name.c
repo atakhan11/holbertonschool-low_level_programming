e4a7ae5c6525afdbd4d714502b2c3645e51490ef
atakhan11 <atakhanwork@gmail.com>
2024-11-23 12:20:40 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
+#include <stdio.h>
+
 /**
- * print_name - prints entered name
- * @name: - input of name
- * @f: - pointer to function
- *
+ *  print_name - print name with function pointer
+ *  @name: pointer
+ *  @f: pointer
+ *  *Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
